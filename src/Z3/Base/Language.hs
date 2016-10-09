af106f03efb2f9ef6ff18962a98adcb774e17288
Zhen Zhang <izgzhen@gmail.com>
2016-10-10 01:17:25 +0200

    Port Logic

--- src/Z3/Base/Language.hs
+++ src/Z3/Base/Language.hs
@@ -13,16 +13,30 @@ class Language repr where
     add :: Num a => repr a -> repr a -> repr a
     equals :: Eq a => repr a -> repr a -> repr Bool
     lessThan, lessEqual, greaterThan, greaterEqual :: Ord a => repr a -> repr a -> repr Bool
+    and_, or_, xor :: repr Bool -> repr Bool -> repr Bool
+    not_ :: repr Bool -> repr Bool
     forall_ :: Z3Sorted a => Z3Sort a -> (repr a -> repr b) -> repr Bool
+    exists :: Z3Sorted a => Z3Sort a -> (repr a -> repr b) -> repr Bool
     member_ :: Ord a => repr a -> repr (S.Set a) -> repr Bool
+
 
 newtype SMTR m e a = SMTR { unSMTR :: SMT m e => m e AST }
 
 mkBiOp :: (AST -> AST -> m e AST) -> SMTR m e a -> SMTR m e b -> SMTR m e c
-mkBiOp f a b = SMTR $ do
+mkBiOp op a b = SMTR $ do
     a' <- unSMTR a
     b' <- unSMTR b
-    f a' b'
+    op a' b'
+
+mkBinder :: Z3Sorted a1 => ([t] -> [Symbol] -> [Sort] -> AST -> m e AST) ->
+                           Z3Sort a1 -> (SMTR m1 e1 a3 -> SMTR m e a2) -> SMTR m e a
+mkBinder op s f = SMTR $ do
+    sym <- mkStringSymbol "x"
+    s' <- sortOf s
+    idx <- mkBound 0 s'
+    local $ do
+        body <- unSMTR (f $ SMTR $ return idx)
+        op [] [sym] [s'] body
 
 instance SMT m e => Language (SMTR m e) where
     lit = SMTR . encode
@@ -35,13 +49,13 @@ instance SMT m e => Language (SMTR m e) where
     greaterThan = mkBiOp mkGt
     greaterEqual = mkBiOp mkGe
 
-    forall_ s f = SMTR $ do
-        sym <- mkStringSymbol "x"
-        s' <- sortOf s
-        idx <- mkBound 0 s'
-        local $ do
-            body <- unSMTR (f $ SMTR $ return idx)
-            mkForall [] [sym] [s'] body
+    and_ = mkBiOp (\a' b' -> mkAnd [a', b'])
+    or_ = mkBiOp (\a' b' -> mkOr [a', b'])
+    xor = mkBiOp mkXor
+    not_ b = SMTR (unSMTR b >>= mkNot)
+
+    forall_ = mkBinder mkForall
+    exists = mkBinder mkExists
 
     member_ e s = SMTR $ do
         eTm <- unSMTR e
